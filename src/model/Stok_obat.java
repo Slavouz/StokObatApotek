4aa3cd0d72d3b048622b140891d7c8ca2e3df813
Slavouz <tmbwpva@protonmail.com>
2022-07-09 23:32:12 +0700

    Revert to Original Workspace

--- src/model/Stok_obat.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Stok_obat {
-    public static final String tabel = "stok_obat";
-    public static final String where = "WHERE";
-    public static final String like = "LIKE";
-    
-    public static final String selectAll = "SELECT * FROM";
-}
